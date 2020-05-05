e9d295d06d9a9d66a76ce340e32dd7fe4bc25999
Connor-John <connor.taylor@icloud.com>
2020-05-05 20:42:54 +1200

    update model and preproc
    - added batch normalisation
    - added data augmentation

--- CIFAR-10/model.py
+++ CIFAR-10/model.py
@@ -1,28 +1,51 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Model
+# Define the model
 class CNN(nn.Module):
   def __init__(self, output_dim):
-    
     super(CNN, self).__init__()
-
-    self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 32, kernel_size = 3, stride = 2)
-    self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, stride = 2)
-    self.conv3 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 2)
-
-    self.fc1 = nn.Linear(128 * 3 * 3, 1024)
-    self.fc2 = nn.Linear(1024, output_dim)
-
-  def forward(self, x):
     
-    out = F.relu(self.conv1(x))
-    out = F.relu(self.conv2(out))
-    out = F.relu(self.conv3(out))
-    out = out.view(-1, 128 * 3 * 3)
-    out = F.dropout(out, p = 0.5)
-    out = F.relu(self.fc1(out))
-    out = F.dropout(out, p = 0.2)
-    out = self.fc2(out)
+    self.conv1 = nn.Sequential(
+        nn.Conv2d(3, 32, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.Conv2d(32, 32, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.MaxPool2d(2))
 
-    return out
+    self.conv2 = nn.Sequential(
+        nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(64),
+        nn.Conv2d(64, 64, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(64),
+        nn.MaxPool2d(2))
+
+    self.conv3 = nn.Sequential(
+        nn.Conv2d(64, 128, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.Conv2d(128, 128, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(128),
+        nn.MaxPool2d(2))
+
+    # Convolutional arithmetic for input
+    self.fc1 = nn.Linear(128 * 4 * 4, 1024)
+    self.fc2 = nn.Linear(1024, output_dim)
+  
+  def forward(self, x):
+
+    x = self.conv1(x)
+    x = self.conv2(x)
+    x = self.conv3(x)
+    x = x.view(x.size(0), -1)
+    x = F.dropout(x, p=0.5)
+    x = F.relu(self.fc1(x))
+    x = F.dropout(x, p=0.2)
+    x = self.fc2(x)
+
+    return x
