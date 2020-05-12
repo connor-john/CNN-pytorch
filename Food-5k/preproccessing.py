1a86c3f6175ccc70f0d3c372697bdedc91a24178
Connor-John <connor.taylor@icloud.com>
2020-05-12 19:39:30 +1200

    Food-5k preproc iter 1

---
+++ Food-5k/preproccessing.py
@@ -0,0 +1,64 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+import numpy as np
+import sys, os
+
+# Get data
+def get_data():
+
+    # Data augmentation
+    train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+            transforms.RandomRotation(degrees=15),
+            transforms.ColorJitter(),
+            transforms.CenterCrop(size=224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    test_transform = transforms.Compose([
+            transforms.Resize(size=256),
+            transforms.CenterCrop(size=224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    # Get Data -- https://mmspg.epfl.ch/downloads/food-image-datasets/
+    # Directories for the data in Keras-style
+    train_dataset = datasets.ImageFolder(
+        'data/train',
+        transform=train_transform
+    )
+
+    test_dataset = datasets.ImageFolder(
+        'data/test',
+        transform=test_transform
+    )
+
+    labels = len(set(train_data.targets))
+
+    return train_data, test_data, labels
+    
+# prep data
+def get_loaders(train, test, batch_size):
+    train_loader = torch.utils.data.DataLoader(dataset = train,
+                                           batch_size = batch_size,
+                                           shuffle = True)
+
+    test_loader = torch.utils.data.DataLoader(dataset = test,
+                                           batch_size = batch_size,
+                                           shuffle = False)
+    
+    return train_loader, test_loader
+
+# make environment
+def make_env(batch_size):
+    
+    train_data, test_data, labels = get_data()
+
+    train_loader, test_loader = get_loaders(train_data, test_data, batch_size)
+
+    print('environment made...')
+    return train_loader, test_loader, labels, test_data
